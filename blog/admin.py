2fd51779ec340425b2ee9549c0a4d46de24cadaa
JaredP45 <52184578+JaredP45@users.noreply.github.com>
2021-08-18 21:52:42 -0500

    Establishing the basis of django-tech-blog project.

---
+++ blog/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Post
+from django_summernote.admin import SummernoteModelAdmin
+
+
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content',)
+
+
+admin.site.register(Post, PostAdmin)
