e682a98fea31a2357ac9c86a6413fe27b1f72b65
JaredP45 <synetos@protonmail.com>
2021-12-15 18:55:52 -0600

    Adjusted css values and colors; add gallery page with temporary notice; updated contact page icons.

--- blog/urls.py
+++ blog/urls.py
@@ -2,12 +2,12 @@
 from . import views
 from django.urls import path
 
-
-from .views import contact_view
+from .views import contact_view, gallery_view
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='blog'),
     path('contact/', contact_view, name='contact'),
+    path('gallery/', gallery_view, name='gallery'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
 
