cc5068aee6751a07c11ea367eeb17fcc9f83b832
JaredP45 <synetos@protonmail.com>
2021-12-24 16:02:45 -0600

    Updated css for footer and gallery; removed sitemap setting in urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,12 +18,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from photologue.sitemaps import GallerySitemap, PhotoSitemap
-
-sitemaps = {
-    'photologue_galleries': GallerySitemap,
-    'photologue_photos': PhotoSitemap,
-}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
