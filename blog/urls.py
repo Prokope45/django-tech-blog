eb4b24b5f51de70bae22420439f1e0f292dcd8bb
JaredP45 <synetos@protonmail.com>
2021-12-24 15:37:18 -0600

    Removed portfolio and opting to use blog for both entries and tutorials; minor adjustments to navbar and footer.

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,6 @@ from .views import index_view, portfolio_view, contact_view, gallery_view
 
 urlpatterns = [
     path('', index_view, name='home'),
-    path('portfolio/', portfolio_view, name='portfolio'),
     path('blog/', views.PostList.as_view(), name='blog'),
     path('contact/', contact_view, name='contact'),
     path('gallery/', gallery_view, name='gallery'),
