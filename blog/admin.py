9b5980a6222e59a1af07dec0e4d5e06b0dfd2cf6
JaredP45 <synetos@protonmail.com>
2022-01-03 12:01:50 -0600

    Removed the permissions for "Add" and "Delete" options for about me & plans models; removed the date field and returned a basic title for the IndexDescription model.

--- blog/admin.py
+++ blog/admin.py
@@ -7,7 +7,15 @@ class PostAdmin(SummernoteModelAdmin):
     summernote_fields = ('content',)
 
 
-admin.site.register(IndexDescription)
+class IndexAdmin(admin.ModelAdmin):
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request):
+        return False
+
+
+admin.site.register(IndexDescription, IndexAdmin)
 admin.site.register(Post, PostAdmin)
 admin.site.register(Image)
 admin.site.register(Contact)
