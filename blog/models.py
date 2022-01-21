094da7277f6fcc6ac4c8543536919c32a18c631c
JaredP45 <synetos@protonmail.com>
2022-01-20 19:11:00 -0600

    Fixed blog post date functionality.

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 from taggit.managers import TaggableManager
 
@@ -31,19 +32,18 @@ class Post(models.Model):
     title = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=50, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
-    updated_on = models.DateTimeField(auto_now=True)
+    updated_on = models.DateField(default=timezone.now)
+    created_on = models.DateField(default=timezone.now)
     content = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     thumb = models.ImageField(blank=True)
+    tag = TaggableManager()
 
     class Meta:
         ordering = ['-created_on']
 
     def __str__(self):
         return self.title
-
-    tag = TaggableManager()
 
 
 # Gallery Page
