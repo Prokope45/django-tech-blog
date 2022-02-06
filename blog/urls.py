91eea76dfb7cdeca5fad7e5a0f7cdbdf83f9887e
JaredP45 <synetos@protonmail.com>
2022-02-06 10:32:46 -0600

    Moved update_server/ url up next to base

--- blog/urls.py
+++ blog/urls.py
@@ -5,11 +5,11 @@ from .views import index_view, contact_view, gallery_view
 
 urlpatterns = [
     path('', index_view, name='home'),
+    path('update_server/', views.update, name='update'),
     path('blog/', views.PostList.as_view(), name='blog'),
     path('contact/', contact_view, name='contact'),
     path('gallery/', gallery_view, name='gallery'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('tags/<slug:tag_slug>/', views.TagIndexView.as_view(), name='posts_by_tag'),
-    path("update_server/", views.update, name="update"),
 ]
 
