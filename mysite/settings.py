12a62de2ad82b93a2612d785017e7f30526c34c8
Jay Paubel <prokope@Jays-Air.attlocal.net>
2024-09-14 23:03:14 -0500

    Implement custom admin panel

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,6 +25,8 @@ ALLOWED_HOSTS = ['www.jpaubel.tech', 'www.prokope.io', 'localhost', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'admin_interface',
+    'colorfield',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
