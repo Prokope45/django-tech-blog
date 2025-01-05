2c0b26adfee5365da9b804c6f981e7ecd3c22973
Jared <jpaubel@pm.me>
2025-01-05 20:59:34 +0000

    Set debug to false

--- mysite/settings.py
+++ mysite/settings.py
@@ -18,7 +18,7 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['www.jpaubel.tech', 'www.prokope.io', 'localhost', '127.0.0.1']
 
