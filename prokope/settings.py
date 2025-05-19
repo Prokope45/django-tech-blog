4f45ed4b94fe5f750ff0ee2ad8df99115076c27c
Jay <52184578+Prokope45@users.noreply.github.com>
2025-05-19 12:14:47 -0500

    Move blog app static files to root static folder. Add new 404 error animation.

--- prokope/settings.py
+++ prokope/settings.py
@@ -18,7 +18,7 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 PRODUCTION = False
 TESTING = not DEBUG and not PRODUCTION
 
@@ -148,9 +148,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
-STATICFILES_DIRS = [BASE_DIR / 'static',]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
