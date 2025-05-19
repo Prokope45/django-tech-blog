2b420debf7fff8160d150f359493ea6d9115ad46
Jay <52184578+Prokope45@users.noreply.github.com>
2025-05-19 12:42:04 -0500

    Remove extra js file and diagnosis CSS problem.

--- static/js/ToTopButton.js
+++ static/js/ToTopButton.js
@@ -1,5 +1,5 @@
 // Get the button
-let topButton = document.getElementById("backToTopBtn");
+var topButton = document.getElementById("backToTopBtn");
                 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
