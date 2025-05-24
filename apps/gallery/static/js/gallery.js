0c8336751c1ae9e6de559456c1f02d7be5a22854
Jay <52184578+Prokope45@users.noreply.github.com>
2025-05-24 18:38:54 -0500

    Separate javascript into two files. Move css to static css file.

--- apps/gallery/static/js/gallery.js
+++ apps/gallery/static/js/gallery.js
@@ -1,63 +1,18 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const select = document.getElementById('city-filter');
-  const form = document.querySelector('form');
-  var elem = document.querySelector('#masonry-container');
-  let msnry;
+const carouselElements = document.querySelectorAll('#hover-carousel');
 
-  // Function to initialize Masonry after images are loaded
-  function initializeMasonry() {
-    // Destroy existing Masonry instance if it exists
-    if (msnry) {
-      msnry.destroy();
-    }
-
-    msnry = new Masonry( elem, {
-      itemSelector: '.gallery_product',
-      columnWidth: '.gallery_product'
-    });
-     // Layout after Masonry is initialized
-    msnry.layout();
-  }
-
-  // Load images first
-  imagesLoaded(elem).on('always', function() {
-    initializeMasonry();
+carouselElements.forEach((carouselEl) => {
+  const carousel = new bootstrap.Carousel(carouselEl, {
+    interval: 1500,
+    ride: false,
+    pause: false,
+    wrap: true
   });
 
-  form.addEventListener('submit', function(event) {
-    // Prevent form submission.
-    event.preventDefault();
-    const value = select.value;
-
-    if (value.toLowerCase() === 'all') {
-      document.querySelectorAll('.gallery_product').forEach(item => {
-        // Callback to display all images and re-layout
-        $(item).show(300, function() {
-          item.classList.remove('hidden');
-          msnry.layout();
-        });
-      });
-    } else {
-      document.querySelectorAll('.gallery_product').forEach(item => {
-        if (!item.classList.contains(value)) {
-          // Callback to hide unrelated images and re-layout
-          $(item).hide(500, function() {
-            item.classList.add('hidden');
-            msnry.layout();
-          });
-        } else {
-          // Callback to display related images and re-layout
-          $(item).show(500, function() {
-            item.classList.remove('hidden');
-            msnry.layout();
-          });
-        }
-      });
-    }
+  carouselEl.addEventListener('mouseenter', () => {
+    carousel.cycle();
   });
 
-  // Trigger form submission once animation is finished.
-  select.addEventListener('change', function() {
-    form.dispatchEvent(new Event('submit'));
+  carouselEl.addEventListener('mouseleave', () => {
+    carousel.pause();
   });
 });
