a9c4144968e8b392623af47a4f71b34bac802170
Jay <52184578+Prokope45@users.noreply.github.com>
2025-05-25 14:39:10 -0500

    Remove temp html code and include previous gallery model implementation.

--- apps/gallery/models.py
+++ apps/gallery/models.py
@@ -10,6 +10,27 @@ from taggit.managers import TaggableManager
 from sortedm2m.fields import SortedManyToManyField
 
 
+class PhotoGallery(models.Model):
+    """Photo gallery model."""
+
+    # TODO: Change format to get Photos directly from
+    #   Photologue, then generate new to enforce pagination
+    galleries = models.ManyToManyField(Gallery)
+    country = models.CharField(max_length=199)
+    content = models.TextField()
+    slug = models.SlugField(max_length=250, allow_unicode=True, blank=True)
+
+    class Meta:
+        """Metadata for PhotoGallery class."""
+        ordering = ["country"]
+        verbose_name = "Gallery"
+        verbose_name_plural = "Galleries"
+
+    def __str__(self):
+        """String representation of name."""
+        return f"{self.country}"
+
+
 class Country(models.Model):
     """A specified country."""
     name = models.CharField(max_length=100, unique=True)
