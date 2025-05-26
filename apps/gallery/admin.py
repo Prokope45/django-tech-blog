341ea4042f8a570d343a267f56a7e4f65ee72d70
Jay <52184578+Prokope45@users.noreply.github.com>
2025-05-26 10:12:02 -0500

    Add upload_zip functionality to admin for CityPhoto

--- apps/gallery/admin.py
+++ apps/gallery/admin.py
@@ -3,6 +3,12 @@
 Author: Jared Paubel
 Version: 0.1
 """
+from django.urls import path
+from django.shortcuts import render
+from django.contrib import helpers
+from photologue.forms import UploadZipForm
+from django.http import HttpResponseRedirect
+from django.utils.translation import gettext_lazy as _
 from django.contrib import admin
 from apps.gallery.models import (
     CountryAlbum,
@@ -66,3 +72,34 @@ class CityPhotoAdmin(BasePhotoAdmin):
     #     Return empty perms dict, hiding the model from admin index.
     #     """
     #     return {}
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path('upload_zip/',
+                 self.admin_site.admin_view(self.upload_zip),
+                 name='photologue_upload_zip')
+        ]
+        return custom_urls + urls
+
+    def upload_zip(self, request):
+        context = {
+            'title': _('Upload a zip archive of photos'),
+            'app_label': self.model._meta.app_label,
+            'opts': self.model._meta,
+            'has_change_permission': self.has_change_permission(request)
+        }
+
+        # Handle form request
+        if request.method == 'POST':
+            form = UploadZipForm(request.POST, request.FILES)
+            if form.is_valid():
+                form.save(request=request)
+                return HttpResponseRedirect('..')
+        else:
+            form = UploadZipForm()
+        context['form'] = form
+        context['adminform'] = helpers.AdminForm(form,
+                                                 list([(None, {'fields': form.base_fields})]),
+                                                 {})
+        return render(request, 'admin/photologue/photo/upload_zip.html', context)
