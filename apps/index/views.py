06f787f015723e6ab3f9e13c597d8862f8dd84a3
Jay <52184578+Prokope45@users.noreply.github.com>
2025-05-28 11:31:37 -0500

    Introduce new lazy load feature, and add gallery preview in index.

--- apps/index/views.py
+++ apps/index/views.py
@@ -9,6 +9,7 @@ from django.http import HttpRequest, HttpResponse
 
 from apps.index.models import Index
 from apps.blog.models import Post
+from apps.gallery.models import CityGallery, CountryAlbum
 
 
 def index_view(request: HttpRequest) -> HttpResponse:
@@ -24,10 +25,14 @@ def index_view(request: HttpRequest) -> HttpResponse:
         'index_data': Index.objects.first(),
         'posts': Post.objects.filter(status=1).order_by('-updated_on')[:2],
     }
-    # if (len(CountryGallery.objects.all()) > 0):
-    #     random_gallery = random.choice(CountryGallery.objects.all())
-    #     context['carousel_gallery_name'] = random_gallery
-    #     context['carousel_photos'] = random.choice(
-    #         random_gallery.galleries.all()
-    #     )
+    if (len(CityGallery.objects.all()) > 0):
+        random_gallery = random.choice(CityGallery.objects.all())
+        city = random_gallery.city
+        context["gallery"] = {
+            "country": CountryAlbum.objects.filter(
+                city_galleries__city=city
+            ).first(),
+            "city": city.name,
+            "photos": random_gallery.city_photos.all()[:10]
+        }
     return render(request, 'index.html', context)
---
+++ static/js/LazyLoader/LazyLoader.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const lazyElements = document.querySelectorAll('[data-lazy]');
+
+    const observer = new IntersectionObserver((entries, obs) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) return;
+
+        const el = entry.target;
+        const type = el.dataset.lazy;
+
+        if (type === 'image') {
+        const spinner = el.closest('[data-masonry-item]')?.querySelector('.spinner');
+        el.src = el.dataset.src;
+        el.onload = () => {
+            el.style.opacity = 1;
+            spinner?.remove();
+            // If using Masonry
+            if (window.masonry) window.masonry.layout();
+        };
+        }
+
+        if (type === 'section') {
+            el.classList.add('in-view');
+        }
+
+        obs.unobserve(el);
+    });
+    });
+
+    lazyElements.forEach(el => observer.observe(el));
+});
