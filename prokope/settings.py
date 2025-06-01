4ccfa8ea76fdb75d31cfb29dff66f43699b7ec70
Jay <52184578+Prokope45@users.noreply.github.com>
2025-06-01 14:29:29 -0500

    Set APP_DIRS to true to allow Django template loader to look into subdirectories in app templates

--- prokope/settings.py
+++ prokope/settings.py
@@ -104,7 +104,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [TEMPLATES_DIRS],
-        'APP_DIRS': False,
+        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
