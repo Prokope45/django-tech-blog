d376e5d37dbc034e9e5722b3804fdea0ba01c5b2
Jay <52184578+Prokope45@users.noreply.github.com>
2025-06-01 14:49:21 -0500

    Change templates settings

--- prokope/settings.py
+++ prokope/settings.py
@@ -103,7 +103,7 @@ ROOT_URLCONF = 'prokope.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [TEMPLATES_DIRS],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -111,11 +111,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-            ],
-            'loaders': [
-                'django.template.loaders.filesystem.Loader',
-                'django.template.loaders.app_directories.Loader',
-            ],
+            ]
         },
     },
 ]
